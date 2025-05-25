830214acdd319465929385b19ccdb0bb2c0e4751
dtaranenko <dtaranenko@nntc.pro>
2025-05-26 01:07:44 +0700

    Set up communication between the server and the client

--- src/ru/taranenko/testDatingApp/back/TestDatingAppRunner.java
+++ src/ru/taranenko/testDatingApp/back/TestDatingAppRunner.java
@@ -1,7 +1,23 @@
 package ru.taranenko.testDatingApp.back;
 
-public class TestDatingAppRunner {
-    public static void main(String[] args){
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class TestDatingAppRunner {
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("localhost", 8800);
+             DataOutputStream rqStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream rsStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+                 while (scanner.hasNextLine()) {
+                     String request = scanner.nextLine();
+                     rqStream.writeUTF(request);
+                     String response = rsStream.readUTF();
+                     System.out.println(response);
+                 }
+        }
     }
 }
---
+++ src/ru/taranenko/testDatingApp/back/model/Commands.java
@@ -0,0 +1,24 @@
+package ru.taranenko.testDatingApp.back.model;
+
+public enum Commands {
+
+    SAVE("save "),
+
+    FIND_BY_ID("findById "),
+
+    FIND_ALL("findAll "),
+
+    UPDATE("update "),
+
+    DELETE("delete ");
+
+    private final String prefix;
+
+    Commands(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+}
