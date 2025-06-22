7072db02d8df3018e0685e05ed852e96f0c222de
dtaranenko <dtaranenko@nntc.pro>
2025-06-23 00:22:24 +0700

    Integrated Lombok for Profile.java

--- src/ru/taranenko/testDatingApp/back/model/Profile.java
+++ src/ru/taranenko/testDatingApp/back/model/Profile.java
@@ -1,5 +1,8 @@
 package ru.taranenko.testDatingApp.back.model;
 
+import lombok.Data;
+
+@Data
 public class Profile {
     private long id;
 
@@ -7,55 +10,4 @@ public class Profile {
     private String name;
     private String surname;
     private String about;
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Override
-    public String toString() {
-        return "Profile{" +
-                " id = " + id +
-                ", email = " + email +
-                ", name = " + name +
-                ", surname = " + surname +
-                ", about = " + about +
-                '}';
-    }
 }
